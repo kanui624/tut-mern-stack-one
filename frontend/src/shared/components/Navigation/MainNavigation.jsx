c8fccf6e971be8595e1831f3f7dcf67d39e453ef
Kanui <marley.kanui@gmail.com>
2020-11-04 10:39:24 -0600

    tut-mern-stack-one - Global commit 10 - Managed Side Drawer State with hooks and Added BackDrop

--- frontend/src/shared/components/Navigation/MainNavigation.jsx
+++ frontend/src/shared/components/Navigation/MainNavigation.jsx
@@ -1,33 +1,47 @@
 // Dependencies
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // Components
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import BackDrop from '../UIElements/BackDrop';
 
 // Css
 import './MainNavigation.css';
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen && <BackDrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span />
           <span />
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">Your Places</Link>
+          <Link to="/">YourPlaces</Link>
         </h1>
-        <nav>
+        <nav className="main-navigation__header-nav">
           <NavLinks />
         </nav>
       </MainHeader>
