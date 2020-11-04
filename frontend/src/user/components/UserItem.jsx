5ed25fd4e77f700a3933513186d9d8532c78aac1
Kanui <marley.kanui@gmail.com>
2020-11-03 20:45:36 -0600

    tut-mern-stack-one - Global commit 02 - Adding User UI

---
+++ frontend/src/user/components/UserItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// CSS
+import './UserItem.css';
+
+const UserItem = () => {
+  return <div></div>;
+};
+
+export default UserItem;
--- frontend/src/user/pages/User.jsx
+++ frontend/src/user/pages/User.jsx
@@ -1,7 +1,20 @@
+// Dependencies
 import React from 'react';
 
+// Components
+import UsersList from '../components/UsersList';
+
 const User = () => {
-  return <h2>Fuckshit</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Kanui',
+      image:
+        'https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default User;
