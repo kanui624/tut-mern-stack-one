78ade041053709c7248df863bdf4079bd73726e9
Kanui <marley.kanui@gmail.com>
2020-11-03 20:28:16 -0600

    tut-mern-stack-one - Global commit 01 - Added React-Router-Dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
+// Pages
+import User from './user/pages/User';
 
 function App() {
-  return <Router></Router>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={User} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
