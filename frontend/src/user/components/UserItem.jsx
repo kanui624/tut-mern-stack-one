2f8a1b72f9f274cfaada4f70bc837b4de1171dda
Kanui <marley.kanui@gmail.com>
2020-11-03 22:14:23 -0600

    tut-mern-stack-one - Global commit 03 - Created User Card with Dummy Data for Now

--- frontend/src/user/components/UserItem.jsx
+++ frontend/src/user/components/UserItem.jsx
@@ -1,10 +1,32 @@
+// Dependencies
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Components
+import Avatar from '../../shared/components/UIElements/Avatar';
+import Card from '../../shared/components/UIElements/Card';
 
 // CSS
 import './UserItem.css';
 
-const UserItem = () => {
-  return <div></div>;
+const UserItem = ({ id, image, name, placeCount }) => {
+  return (
+    <li className="user-item">
+      <Card className="user-item__content">
+        <Link to={`/${id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h3>
+              {placeCount} {placeCount === 1 ? 'Place' : 'Places'}
+            </h3>
+          </div>
+        </Link>
+      </Card>
+    </li>
+  );
 };
 
 export default UserItem;
