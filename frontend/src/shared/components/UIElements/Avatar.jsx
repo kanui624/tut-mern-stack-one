dbac1e42f74821248323000e398889ad4f45cd03
Kanui <marley.kanui@gmail.com>
2020-11-03 22:21:29 -0600

    tut-mern-stack-one - Global commit 04 - User Card Refactor

--- frontend/src/shared/components/UIElements/Avatar.jsx
+++ frontend/src/shared/components/UIElements/Avatar.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 
 import './Avatar.css';
 
-const Avatar = (props) => {
+const Avatar = ({ className, style, image, alt, width, height }) => {
   return (
-    <div className={`avatar ${props.className}`} style={props.style}>
-      <img
-        src={props.image}
-        alt={props.alt}
-        style={{ width: props.width, height: props.width }}
-      />
+    <div className={`avatar ${className}`} style={style}>
+      <img src={image} alt={alt} style={{ width: width, height: height }} />
     </div>
   );
 };
