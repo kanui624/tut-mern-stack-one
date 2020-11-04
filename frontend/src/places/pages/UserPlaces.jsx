91f4deab68a607be758598224b461ca5928891fa
Kanui <marley.kanui@gmail.com>
2020-11-04 11:25:26 -0600

    tut-mern-stack-one - Global commit 13 - Made Loaded Places Dynamic by referenceing query data to Load User specific Places

--- frontend/src/places/pages/UserPlaces.jsx
+++ frontend/src/places/pages/UserPlaces.jsx
@@ -1,5 +1,6 @@
 // Dependencies
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 // Components
 import PlaceList from '../components/PlaceList';
@@ -11,7 +12,7 @@ const UserPlaces = () => {
   const DUMMY_PLACES = [
     {
       id: 'p1',
-      title: 'Empire State Building',
+      title: 'Empire A Building',
       description: 'One of the most famous sky scrapers in the world!',
       imageUrl:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
@@ -24,7 +25,7 @@ const UserPlaces = () => {
     },
     {
       id: 'p2',
-      title: 'Empire State Building',
+      title: 'Empire B Building',
       description: 'One of the most famous sky scrapers in the world!',
       imageUrl:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
@@ -36,7 +37,12 @@ const UserPlaces = () => {
       creator: 'u2',
     },
   ];
-  return <PlaceList places={DUMMY_PLACES} />;
+
+  const userId = useParams().userId;
+  const userSpecificPlaces = DUMMY_PLACES.filter(
+    ({ creator }) => creator === userId
+  );
+  return <PlaceList places={userSpecificPlaces} />;
 };
 
 export default UserPlaces;
